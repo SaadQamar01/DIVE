541ff8db577d56639ce9ea3e097f82b9d86b5fde
Syed-Saad-Qamar <syed.saad@iomechs.com>
2018-06-23 13:14:12 +0500

    add portfolio routing

--- src/views/Portfolio/Portfolio.jsx
+++ src/views/Portfolio/Portfolio.jsx
@@ -3,9 +3,13 @@ import React from "react";
 import classNames from "classnames";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
-
+import Button from "components/CustomButtons/Button.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
 import profilePageStyle from "assets/jss/material-kit-react/views/profilePage.jsx";
-
+import { Route } from "react-router";
+import { Link, withRouter } from "react-router-dom";
+import portfolioRoutes from "../../routes/portfolioRoutes";
 
 class Portfolio extends React.Component {
   render() {
@@ -17,10 +21,30 @@ class Portfolio extends React.Component {
     );
     return (
       <div>
-        Portfolio
+        <GridContainer>
+          <GridItem>
+            <div style={{ marginLeft: '135px', marginTop: '25px' }}>
+              <Link to={"/dashboard/portfolio"} ><Button style={{ backgroundColor: '#fff', color: '#000' }}>Projects</Button></Link>
+              <Link to={"/dashboard/portfolio/cources"} ><Button style={{ backgroundColor: '#fff', color: '#000' }}>Courses</Button></Link>
+              <Link to={"/dashboard/portfolio/liked"} ><Button style={{ backgroundColor: '#fff', color: '#000' }}>Liked</Button></Link>
+            </div>
+          </GridItem>
+        </GridContainer>
+        <GridContainer>
+          <GridItem>
+            {portfolioRoutes.map((prop, key) => {
+              if (prop.path == '/dashboard/portfolio') {
+                return <Route exact path={prop.path} key={key} component={prop.component} />;
+              }
+              else {
+                return <Route path={prop.path} key={key} component={prop.component} />;
+              }
+            })}
+          </GridItem>
+        </GridContainer>
       </div>
     );
   }
 }
 
-export default withStyles(profilePageStyle)(Portfolio);
+export default withRouter(withStyles(profilePageStyle)(Portfolio));
