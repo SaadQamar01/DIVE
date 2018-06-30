610cf633555a6c23c249f70e445621f71554e3ac
Mohammad-Khalid23 <m.k.ayub23@gmail.com>
2018-06-30 14:20:36 +0500

    added gitignore file

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,4 @@ import * as types from '../constants/ActionTypes';
 
 export * from './auth';
 export * from './user'
+export * from './education'
--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,5 +1,5 @@
 const APPCONFIG = {
-    API_URL: 'https://dive-web.herokuapp.com/',
+    API_URL: 'https://powerful-bastion-24919.herokuapp.com/v1/api/',
     SOCKET_URL: 'http://localhost:8000'
 };
 
